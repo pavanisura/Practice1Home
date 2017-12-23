840c6d0cd31fec4f69d107f5063afb4ffa69a24f
pavanisura <pavani.sura267@gmail.com>
2017-12-23 12:34:16 +0530

    this is my practice Program

---
+++ src/Operations.java
@@ -0,0 +1,28 @@
+
+public class Operations {
+
+	public static void main(String[] args)
+	{
+
+	      int num1 = 10;
+	      int num2 = 20;
+
+	      num2 = num1;
+	      System.out.println("= Output: "+num2);
+
+	      num2 += num1;
+	      System.out.println("+= Output: "+num2);
+		      
+	      num2 -= num1;
+	      System.out.println("-= Output: "+num2);
+		      
+	      num2 *= num1;
+	      System.out.println("*= Output: "+num2);
+		      
+	      num2 /= num1;
+	      System.out.println("/= Output: "+num2);
+		      
+	      num2 %= num1;
+	      System.out.println("%= Output: "+num2);
+	   }
+	}
